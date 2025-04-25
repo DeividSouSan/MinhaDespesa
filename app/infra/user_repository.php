23991a041fa880fc1f4880230203195804568335
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-04-25 20:58:58 -0300

    feat: add `getByEmail` and `getByUsername`  methods

--- app/infra/user_repository.php
+++ app/infra/user_repository.php
@@ -26,4 +26,29 @@ class UserRepository
 
         $result = $stmt->execute();
     }
+
+    function getByEmail(string $email)
+    {
+        $stmt = $this->database->prepare("
+        SELECT * FROM User WHERE email = ?;
+        ");
+
+        $stmt->bind_param("s", $email);
+
+        $stmt->execute();
+
+        return $stmt->get_result()->fetch_assoc();
+    }
+    function getByUsername(string $username)
+    {
+        $stmt = $this->database->prepare("
+        SELECT * FROM User WHERE username = ?;
+        ");
+
+        $stmt->bind_param("s", $username);
+
+        $stmt->execute();
+
+        return $stmt->get_result()->fetch_assoc();
+    }
 }
