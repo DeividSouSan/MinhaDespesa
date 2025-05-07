60e939475e676eccbe50c8b28448e025ee907fe9
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-05-07 13:34:05 -0300

    feature: add `home` page into router

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 match ($path) {
     '/' => require '../app/pages/home.php',
+    '/home' => require '../app/pages/home.php',
     '/register' => require '../app/pages/register.php',
     '/register/confirm' => require '../app/pages/confirm-register.php',
     '/login' => require '../app/pages/login.php',
