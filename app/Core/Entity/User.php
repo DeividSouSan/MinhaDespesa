35f3861d8ebfca410ecf5e7e8e0fb01764771666
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-05-11 15:16:59 -0300

    refactor: add `id` field into `User` entity

--- app/Core/Entity/User.php
+++ app/Core/Entity/User.php
@@ -1,13 +1,15 @@
 <?php
 class User
 {
+    public string $id;
     public string $username;
     public string $email;
     public string $password;
     public ?string $token;
 
-    function __construct(string $username, string $email, string $password, ?string $token)
+    function __construct(string $id, string $username, string $email, string $password, ?string $token)
     {
+        $this->id = $id;
         $this->username = $username;
         $this->email = $email;
         $this->password = $password;
@@ -17,6 +19,7 @@ class User
     static function fromArray(array $array)
     {
         return new User(
+            $array['id'],
             $array['username'],
             $array['email'],
             $array['password_hash'],
