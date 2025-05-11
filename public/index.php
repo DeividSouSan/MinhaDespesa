375b4943320076dba6df1c84bf2adc8d1259e21f
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-05-11 18:25:00 -0300

    feature: implement `access control` to handle private and public routes access

--- public/index.php
+++ public/index.php
@@ -4,13 +4,21 @@ require '../app/Infra/Database/database.php';
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 session_start();
+$user_is_logged = !empty($_SESSION['UID']);
 
-$not_in_session = !isset($_SESSION['email']);
-$need_auth = in_array($path, ['/finances']);
+$public_routes = ['/', '/home', '/login', '/register', '/register/confirm'];
+$private_routes = ['/finances'];
 
-if ($not_in_session and $need_auth) {
-    header('Location: /login');
-} // quebrado
+// user_in_public_route and user_is_logged -> private_route
+// user_in_public_route and user_is_not_logged -> skip
+// user_in_private_route and user_is_logged -> skip
+// user_in_private_route and user_is_not_logged -> public_route
+
+if (in_array($path, $public_routes) && $user_is_logged) {
+    header('Location: /finances');
+} else if (in_array($path, $private_routes) && !$user_is_logged) {
+    header('Location: /home');
+}
 
 match ($path) {
     '/' => require '../app/Presentation/View/home.php',
